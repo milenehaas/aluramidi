763ba73a4a4e24fa3cc47987d7a640834a736820
Milene Haas <118367965+milenehaas@users.noreply.github.com>
2023-10-19 11:00:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert('va estuda')
+alert('essa pagina contém virus')
+
+function tocaSomPom() {document.querySelector('#som_tecla_pom').play()}
+
+
+
+document.querySelector('.tecla_pom');
+document.querySelector('.tecla_clap');
+document.querySelector('.tecla_tim');
+document.querySelector('.tecla_puff');
+document.querySelector('.tecla_splash');
+document.querySelector('.tecla_toim');
+document.querySelector('.tecla_psh');
+document.querySelector('.tecla_tic');
+document.querySelector('.tecla_tom');
+document.querySelector('#som_tecla_pom').play 
