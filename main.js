689ab6055a6abffeaa299495bd2b94fba1c9c2ec
Milene Haas <118367965+milenehaas@users.noreply.github.com>
2023-10-26 10:58:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 alert('essa pagina contém virus')
 
+
+
+
+
+
 function tocaSomPom() {document.querySelector('#som_tecla_pom').play()}
-
-
-
 document.querySelector('.tecla_pom');
+
+function tocaSomclap() {document.querySelector('#som_tecla_clap').play()}
 document.querySelector('.tecla_clap');
-document.querySelector('.tecla_tim');
+
+function tocaSomtim() {document.querySelector('#som_tecla_tim').play()}
+document.querySelector('.tedcla_tim');
+function tocaSompuff() {document.querySelector('#som_tecla_puff').play()}
 document.querySelector('.tecla_puff');
+function tocaSomsplash() {document.querySelector('#som_tecla_splash').play()}
 document.querySelector('.tecla_splash');
+function tocaSomtoim() {document.querySelector('#som_tecla_toim').play()}
 document.querySelector('.tecla_toim');
+function tocaSompsh() {document.querySelector('#som_tecla_psh').play()}
 document.querySelector('.tecla_psh');
+function tocaSomtic() {document.querySelector('#som_tecla_tic').play()}
 document.querySelector('.tecla_tic');
+function tocaSomtom() {document.querySelector('#som_tecla_tom').play()}
 document.querySelector('.tecla_tom');
-document.querySelector('#som_tecla_pom').play 
+
+
+const listadeteclas = document.querySelectorAll('.tecla')
